ef3fc5c9bab815144e2f871cf59b776c37524b57
ThanhDatThanhDat <dt2229992@gmail.com>
2024-11-22 17:54:33 +0700

    ss4-3

---
+++ ss4-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Nhap mot so nguyen: ");
+    scanf("%d", &num);
+
+    if (num % 3 == 0 && num % 5 == 0) {
+        printf("So %d chia het cho co 3 và 5.\n", num);
+    } else if (num % 3 == 0) {
+        printf("So %d chia het cho 3.\n", num);
+    } else if (num % 5 == 0) {
+        printf("So %d chia het cho 5.\n", num);
+    } else {
+        printf("So %d khong chia het cho ca 3 va 5.\n", num);
+    }
+
+    return 0;
+}
